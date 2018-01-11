8f2028e15a308a73aede903aa82a13419694a469
a-basitq <abdul.basit.atd@gmail.com>
2018-01-12 02:32:18 +0500

    Updated config

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var cells = require('./routes/cells');
 var authMiddleware = require('./routes/middlewares/authMiddleware');
 
 var app = express();
+app.use('/', express.static(__dirname + '/'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'jwt': 'spreadsheet',
-    'db': 'mongodb://localhost/spreadsheet-DB'
+    // 'db': 'mongodb://localhost/spreadsheet-DB'
+    'db': 'mongodb://abasitq:abasitq@ds249707.mlab.com:49707/spreadsheet-db'
 };
